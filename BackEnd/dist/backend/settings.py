13788e1ffcf7632a5b3abd98cdfa6200138b6f16
pramod kumar <pramodkumarbandi13@gmail.com>
2026-02-03 15:52:40 +0530

    changed settings.py

--- BackEnd/dist/backend/settings.py
+++ BackEnd/dist/backend/settings.py
@@ -119,13 +119,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dibet_db',
-        'USER':'postgres',
-        'PASSWORD':'dideccan',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 AUTH_USER_MODEL="user.User"
 # Password validation
